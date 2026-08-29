2be78643dd3d6055afe2edd14bdfcc30d8d26121
Amartya Upmanyu <upmanyu07amartya@gmail.com>
2026-08-29 17:44:13 +0530

    login component + api call using axios

--- README.md
+++ README.md
@@ -7,6 +7,12 @@
  - Create BrowserRouter > Routes > Route > Child Routes
  - Use Outlet to render child routes.
  - Create a Footer
+ - Create a login page
+ - Install axios
+ - Make login API call
+ - use cors middleware in backend
+ - whitelist the frontend url in cors in backend - to see the cookie in the browser
+ -
 
 
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import axios from "axios";
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const handleLogin = async () => {
+    try {
+      const data = await axios.post(
+        "http://localhost:3000/login",
+        {
+          email,
+          password,
+        },
+        { withCredentials: true },
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center bg-base-400 m-10 p-4">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input
+          type="email"
+          className="input"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label className="label">Password</label>
+        <input
+          type="password"
+          className="input"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="btn btn-neutral mt-4" onClick={handleLogin}>
+          Login
+        </button>
+      </fieldset>
+    </div>
+  );
+};
+
+export default Login;
