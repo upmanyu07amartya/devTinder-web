571b8e237dd1ab8f6146aa92e672d1b78b51a377
Amartya Upmanyu <upmanyu07amartya@gmail.com>
2026-08-30 11:13:43 +0530

    fedSlice + getFeed api call

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -16,7 +16,6 @@ const Body = () => {
       const res = await axios.get(BASE_URL + "/profile/view", {
         withCredentials: true,
       });
-      console.log(res.data);
       dispatch(addUser(res.data));
     } catch (err) {
       if (err.status === 401) {
